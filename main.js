09f71d4d7f8151471adc0105eef3c75fb475e9e4
Shubhan28 <83396802+Shubhan28@users.noreply.github.com>
2021-04-30 21:13:36 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,56 @@
+
+function show(message)
+{
+    console.log(message);
+}
+prediction1 = "";
+prediction2 = "";
+Webcam.set({
+    width : 350,
+    height : 300,
+    image_format : 'jpeg',
+    jpeg_quality : 90
+});
+camera = document.getElementById("camera");
+Webcam.attach('#camera');
+function take_snapshot()
+{
+    Webcam.snap(function(data_url){
+        document.getElementById("result").innerHTML = '<img id="captured_image" src="'+data_url+'">';
+    });       
+    
+}
+show(ml5.version);
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/rF30Dar7o/model.json', modelLoaded);
+function modelLoaded()
+{
+    show("Model Loaded!");
+}
+function speak()
+{
+    var synthesis = window.speechSynthesis;
+    speak_data = "I found you"+prediction1;
+    var utter = new SpeechSynthesisUtterance(speak_data);
+    synthesis.speak(utter);
+
+}
+function check()
+{
+    show("Checking...");
+    img = document.getElementById("captured_image");
+    classifier.classify(img, get_results);
+}
+function get_results(error, results)
+{
+    if(error)
+    {
+        show("Error, could not recognise the image!!");
+    }else{
+        show("Checked!!");
+        show(results);
+        prediction1 = results[0].label;
+        prediction2 = results[1].label;
+        speak();
+        
+    }
+}
